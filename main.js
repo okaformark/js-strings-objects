199056a9d1a6ccfd271ba5b8db47132a6a3aaba9
Mark Okafor <okaformark@gmail.com>
2019-03-04 21:36:57 -0600

    add html js

--- main.js
+++ main.js
@@ -23,3 +23,49 @@ printToDom('thirsty', iAmThirsty(67));
 
 const quote ='winter is coming';
 console.log(quote.indexOf('is'));
+
+const catName=()=>{
+    return 'killer';
+};
+const bucketOfCats = {
+    cat1: 'fluffy',
+    cat2: 5,
+    cat3: catName,
+    cat4: {
+        water: 'nope',
+        sunlight: 'yep'
+    }
+};
+//dot notation to access function
+console.log(bucketOfCats.cat2);
+console.log(bucketOfCats.cat4.water);
+
+//bracket notation
+console.log(bucketOfCats['cat2']);
+
+let hitchhikers_guide={
+    characters: ["zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "don't panic",
+    random_facts: {
+        copies_sold: 140000,
+        formats: ["radio", "tv", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life:42
+        }
+    }
+}
+console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
+
+let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010",
+    }
+
+console.log('our company lawyer is' + " "+ employee.name);
+console.log(employee['name']+ " was hired "+employee.hire_date);
+employee.vacation_days = 20;
+
+let eom = "employee_of_the_month";
+employee[eom] = false;
